ad666cda0c80cf5e0bce27723e023e902c294698
m7md07sn <mohamedhassan_toty@hotmail.com>
2017-01-31 12:14:15 +0200

    Initial commit

---
+++ app/src/main/java/com/spellad/testtrain/Testing.java
@@ -0,0 +1,24 @@
+package com.spellad.testtrain;
+
+/**
+ * Created by m7md07sn on 31/01/2017.
+ */
+
+public class Testing {
+
+    public Testing() {
+    }
+
+
+    public boolean compare(int a, int b) {
+        return !(a < b);
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public String printStr() {
+        return "Hello";
+    }
+}
---
+++ app/src/test/java/com/spellad/testtrain/TestingTest.java
@@ -0,0 +1,38 @@
+package com.spellad.testtrain;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by m7md07sn on 31/01/2017.
+ */
+public class TestingTest extends TestCase {
+    Testing testing;
+
+    public void setUp() throws Exception {
+        super.setUp();
+        testing = new Testing();
+    }
+
+    public void testCompare() throws Exception {
+
+        int a = 10,
+                b = 20;
+
+        boolean f = testing.compare(a, b);
+        boolean expected = false;
+//        assertFalse(f);
+        assertEquals(expected, f);
+    }
+
+    public void testSum() throws Exception {
+
+        int a=10,b=20;
+        assertEquals(testing.sum(a,b),20);
+
+    }
+
+    public void testPrintStr() throws Exception {
+
+    }
+
+}
